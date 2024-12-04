65994d56513f5c805375fce8e9cebf29ea83967e
PRAKASH-VD <prakashdjp4@gmail.com>
2024-12-04 18:29:36 +0530

    Initial commit

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.get('/', (req, res) => {
 });
 
 //const PORT = process.env.PORT || 3000;
-const PORT = process.env.PORT || 5001; // Use a different port
+// const PORT = process.env.PORT || 5001; // Use a different port
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
